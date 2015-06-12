e369f2b248fbc6a22309bad3eecebed7d6b448f8
Toru Ogawa <Hakuyume@users.noreply.github.com>
2015-06-12 17:32:08 +0900

    画像を読み込んで表示

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc < 2)
+    return 1;
+  auto input = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  if (input.empty())
+    return 1;
+  cv::namedWindow("input", cv::WINDOW_AUTOSIZE);
+  cv::imshow("input", input);
+
+  cv::waitKey(0);
+
   return 0;
 }
