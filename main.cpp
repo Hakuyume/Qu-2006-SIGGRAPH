ec487b1a1fff2dafcc19e4d49c9e739f32152384
Toru Ogawa <Hakuyume@users.noreply.github.com>
2015-06-12 20:48:26 +0900

    画像の一部のみにフィルタをかけるようにした

--- main.cpp
+++ main.cpp
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
   cv::namedWindow("input", cv::WINDOW_AUTOSIZE);
   cv::imshow("input", input);
 
+  cv::Mat select = cv::Mat::zeros(input.size(), CV_8U);
+  cv::circle(select, cv::Point(150, 120), 20, cv::Scalar::all(0xff), -1);
+  cv::namedWindow("select", cv::WINDOW_AUTOSIZE);
+  cv::imshow("select", input & select);
+
   auto gabor = cv::getGaborKernel(cv::Size(25, 25), 2, CV_PI / 2, 5, 1);
   cv::Mat result;
-  cv::filter2D(input, result, -1, gabor);
+  cv::filter2D(input & select, result, -1, gabor);
 
   cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
   cv::imshow("result", result);
