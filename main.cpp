d25421d1b1056e2d4e1c6c183414aa00b482ffae
Toru Ogawa <Hakuyume@users.noreply.github.com>
2015-06-15 10:37:10 +0900

    連続する似たパターンの領域を抽出してみた

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@
 
 #include "pattern.hpp"
 
+void expand(const cv::Mat &src, cv::Mat &dst, const pattern::Feature &feature, const cv::Point &pos)
+{
+  if (dst.at<unsigned char>(pos))
+    return;
+
+  if ((pattern::getFeature(src, pos) - feature).squaredNorm() > 10000)
+    return;
+
+  dst.at<unsigned char>(pos) = 0xff;
+
+  expand(src, dst, feature, pos + cv::Point(-1, 0));
+  expand(src, dst, feature, pos + cv::Point(+1, 0));
+  expand(src, dst, feature, pos + cv::Point(0, -1));
+  expand(src, dst, feature, pos + cv::Point(0, +1));
+}
+
 int main(int argc, char *argv[])
 {
   if (argc < 2)
@@ -13,41 +29,26 @@ int main(int argc, char *argv[])
   auto input = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
   if (input.empty())
     return 1;
+
   cv::namedWindow("input", cv::WINDOW_AUTOSIZE);
   cv::imshow("input", input);
 
   std::vector<cv::Point> points{
       cv::Point(150, 120),
       cv::Point(175, 105),
-      cv::Point(200, 90),
+      cv::Point(200, 90)};
 
-      cv::Point(110, 240),
-      cv::Point(130, 230),
-      cv::Point(150, 220),
+  pattern::Feature sum = pattern::Feature::Zero();
+  for (auto &p : points)
+    sum += pattern::getFeature(input, p);
+  const auto userFeature = sum / points.size();
 
-      cv::Point(150, 300),
-      cv::Point(165, 275),
-      cv::Point(180, 250),
+  cv::Mat result = cv::Mat::zeros(input.size(), CV_8U);
 
-      cv::Point(45, 175),
-      cv::Point(50, 200),
-      cv::Point(55, 225)};
-
-  auto result = input.clone();
-  std::vector<pattern::Feature> features;
-
-  for (auto &p : points) {
-    cv::circle(result, p, 5, cv::Scalar(0), -1);
-
-    features.push_back(pattern::getFeature(input, p));
-  }
+  expand(input, result, userFeature, points.at(0));
 
   cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
-  cv::imshow("result", result);
-
-  for (size_t i = 0; i < features.size(); i++)
-    for (size_t j = i + 1; j < features.size(); j++)
-      std::cout << i << "-" << j << "\t" << (features.at(i) - features.at(j)).squaredNorm() << std::endl;
+  cv::imshow("result", input & result);
 
   cv::waitKey(0);
 
